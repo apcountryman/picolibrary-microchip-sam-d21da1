0002d7374cee18677062d28ecf5df6ef32e51287
Andrew Countryman <apcountryman@gmail.com>
2024-01-08 13:11:58 -0800

    Update picolibrary-arm-cortex-m0plus (#52)
    Resolves #51 (Update picolibrary-arm-cortex-m0plus).

---
+++ include/picolibrary/arm/cortex/m0plus/implementation/interrupt/vectors.h
@@ -0,0 +1,171 @@
+/**
+ * picolibrary-microchip-sam-d21da1
+ *
+ * Copyright 2023, Andrew Countryman <apcountryman@gmail.com> and the
+ * picolibrary-microchip-sam-d21da1 contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/**
+ * \file
+ * \brief picolibrary::Arm::Cortex::M0PLUS implementation interrupt vectors.
+ */
+
+/**
+ * \brief PM0 handler.
+ */
+Handler pm0_handler;
+
+/**
+ * \brief SYSCTRL0 handler.
+ */
+Handler sysctrl0_handler;
+
+/**
+ * \brief WDT0 handler.
+ */
+Handler wdt0_handler;
+
+/**
+ * \brief RTC0 handler.
+ */
+Handler rtc0_handler;
+
+/**
+ * \brief EIC0 handler.
+ */
+Handler eic0_handler;
+
+/**
+ * \brief NVMCTRL0 handler.
+ */
+Handler nvmctrl0_handler;
+
+/**
+ * \brief DMAC0 handler.
+ */
+Handler dmac0_handler;
+
+/**
+ * \brief USB0 handler.
+ */
+Handler usb0_handler;
+
+/**
+ * \brief EVSYS0 handler.
+ */
+Handler evsys0_handler;
+
+/**
+ * \brief SERCOM0 handler.
+ */
+Handler sercom0_handler;
+
+/**
+ * \brief SERCOM1 handler.
+ */
+Handler sercom1_handler;
+
+/**
+ * \brief SERCOM2 handler.
+ */
+Handler sercom2_handler;
+
+/**
+ * \brief SERCOM3 handler.
+ */
+Handler sercom3_handler;
+
+/**
+ * \brief SERCOM4 handler.
+ */
+Handler sercom4_handler;
+
+/**
+ * \brief SERCOM5 handler.
+ */
+Handler sercom5_handler;
+
+/**
+ * \brief TCC0 handler.
+ */
+Handler tcc0_handler;
+
+/**
+ * \brief TCC1 handler.
+ */
+Handler tcc1_handler;
+
+/**
+ * \brief TCC2 handler.
+ */
+Handler tcc2_handler;
+
+/**
+ * \brief TC3 handler.
+ */
+Handler tc3_handler;
+
+/**
+ * \brief TC4 handler.
+ */
+Handler tc4_handler;
+
+/**
+ * \brief TC5 handler.
+ */
+Handler tc5_handler;
+
+/**
+ * \brief TC6 handler.
+ */
+Handler tc6_handler;
+
+/**
+ * \brief TC7 handler.
+ */
+Handler tc7_handler;
+
+/**
+ * \brief ADC0 handler.
+ */
+Handler adc0_handler;
+
+/**
+ * \brief AC0 handler.
+ */
+Handler ac0_handler;
+
+/**
+ * \brief DAC0 handler.
+ */
+Handler dac0_handler;
+
+/**
+ * \brief PTC0 handler.
+ */
+Handler ptc0_handler;
+
+/**
+ * \brief I2S0 handler.
+ */
+Handler i2s0_handler;
+
+/**
+ * \brief AC1 handler.
+ */
+Handler ac1_handler;
+
+/**
+ * \brief TCC3 handler.
+ */
+Handler tcc3_handler;
